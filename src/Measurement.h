ce35694765d3519e7bdc9e8393ccedb07266a4d4
Arnold deVos <adv@bgsig.com>
2021-05-30 23:39:57 +1000

    scalar Meaurement template

--- src/Measurement.h
+++ src/Measurement.h
@@ -11,12 +11,30 @@ struct Measurement3D
     }
     Measurement3D(): Measurement3D(0, 0, 0, 0) {}
 
-    unsigned char _buffer[sizeof(A)*3 + sizeof(unsigned short)];
-
     A *ap(unsigned i) { return reinterpret_cast<A*>(_buffer + sizeof(A)*i); }
     unsigned short *tp() { return reinterpret_cast<unsigned short*>(_buffer + sizeof(A)*3); }
 
+    unsigned char _buffer[sizeof(A)*3 + sizeof(unsigned short)];
     uint8_t* buffer() { return _buffer; } // endian - shmedian
     uint16_t length() { return sizeof(_buffer); } 
 
+};
+
+template<typename A>
+struct Measurement
+{
+    Measurement(A x, unsigned short t) 
+    {
+        *ap() = x;
+        *tp() = t;
+    }
+
+    Measurement() : Measurement(0, 0) {}
+
+    A *ap() { return reinterpret_cast<A*>(_buffer); }
+    unsigned short *tp() { return reinterpret_cast<unsigned short*>(_buffer + sizeof(A)); }
+
+    unsigned char _buffer[sizeof(A) + sizeof(unsigned short)];
+    uint8_t* buffer() { return _buffer; } // endian - shmedian
+    uint16_t length() { return sizeof(_buffer); } 
 };
